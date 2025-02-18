55451d182e7b51420b63e98fd5853dfb399da26b
Waffle <151867877+Wueffi@users.noreply.github.com>
2025-02-18 16:55:52 +0100

    Add files via upload

---
+++ DecoderGenerator.py
@@ -0,0 +1,105 @@
+import mcschematic
+
+
+def create_decoder_schematic(input_patterns, filename="Decoder.schem"):
+    schem = mcschematic.MCSchematic()
+    spacing = 2
+    x = 0  # Starting X
+    z = 1  # Starting Z
+
+    for input_pattern in input_patterns:
+        y = 0  # Starting Y
+        for num in input_pattern:
+            if num == 1:
+                schem.setBlock((x, y, z), "minecraft:redstone_wall_torch[facing=north]")  # ON
+                schem.setBlock((x, y + spacing, z), "minecraft:repeater[facing=south,delay=1,locked=false,powered=false]")  # OFF
+            elif num == 2:
+                schem.setBlock((x, y, z), "minecraft:repeater[facing=south,delay=1,locked=false,powered=false]")  # OFF
+                schem.setBlock((x, y + spacing, z), "minecraft:redstone_wall_torch[facing=north]")  # ON
+            elif num == 3:
+                schem.setBlock((x, y, z), "minecraft:redstone_wall_torch[facing=north]")  # ON
+                schem.setBlock((x, y + spacing, z), "minecraft:redstone_wall_torch[facing=north]")  # ON
+            elif num == 0:
+                schem.setBlock((x, y, z), "minecraft:repeater[facing=south,delay=1,locked=false,powered=false]")  # OFF
+                schem.setBlock((x, y + spacing, z),
+                               "minecraft:repeater[facing=south,delay=1,locked=false,powered=false]")  # OFF
+
+            schem.setBlock((x, y + 1, z), "minecraft:smooth_quartz_slab[type=top]")
+            schem.setBlock((x, y - 1, z), "minecraft:smooth_quartz_slab[type=top]")
+            y += spacing * 2  # Move up
+
+        x -= spacing  # Spacing
+
+    schem.save("./schems", "Decoder", mcschematic.Version.JE_1_20_1)
+    print(f"Schematic saved as {filename}")
+
+def create_lut_schematic(input_patterns, filename="LUT.schem"):
+    schem = mcschematic.MCSchematic()
+    spacing = 4
+    x = 0  # Starting X
+    z = 0  # Starting Z
+
+    for input_pattern in input_patterns:
+        y = 0  # Starting Y
+        for num in input_pattern:
+            if num == 2:
+                schem.setBlock((x, y, z), "minecraft:redstone_wall_torch[facing=north]")  # ON
+            elif num == 3:
+                schem.setBlock((x, y + spacing, z), "minecraft:redstone_wall_torch[facing=north]")
+            elif num == 4:
+                schem.setBlock((x, y, z), "minecraft:redstone_wall_torch[facing=north]")
+                schem.setBlock((x, y + spacing, z), "minecraft:redstone_wall_torch[facing=north]")
+            elif num == 5:
+                schem.setBlock((x, y + 2 * spacing, z), "minecraft:redstone_wall_torch[facing=north]")
+            elif num == 6:
+                schem.setBlock((x, y, z), "minecraft:redstone_wall_torch[facing=north]")
+                schem.setBlock((x, y + 2 * spacing, z), "minecraft:redstone_wall_torch[facing=north]")
+            elif num == 7:
+                schem.setBlock((x, y + spacing, z), "minecraft:redstone_wall_torch[facing=north]")
+                schem.setBlock((x, y + 2 * spacing, z), "minecraft:redstone_wall_torch[facing=north]")
+            elif num == 8:
+                schem.setBlock((x, y, z), "minecraft:redstone_wall_torch[facing=north]")
+                schem.setBlock((x, y + spacing, z), "minecraft:redstone_wall_torch[facing=north]")
+                schem.setBlock((x, y + 2 * spacing, z), "minecraft:redstone_wall_torch[facing=north]")
+
+            x += 2
+            if x == 16:
+                x = 0
+        z += 2
+
+    schem.save("./schems", "LUT", mcschematic.Version.JE_1_20_1)
+    print(f"Schematic saved as {filename}")
+
+input_data = {
+     (3, 3, 3, 3, 3): (1, 1, 1, 1, 2, 2, 2, 2), (1, 3, 3, 3, 3): (3, 3, 3, 3, 4, 4, 4, 4),
+     (1, 1, 3, 3, 3): (5, 5, 5, 5, 6, 6, 6, 6), (1, 1, 2, 3, 3): (1, 1, 1, 1, 3, 3, 3, 3),
+     (1, 1, 2, 0, 3): (7, 7, 7, 7, 8, 8, 8, 8), (1, 1, 1, 3, 3): (1, 1, 1, 1, 3, 3, 3, 3),
+     (1, 1, 1, 1, 3): (1, 1, 1, 1, 5, 5, 5, 5), (0, 3, 3, 3, 3): (3, 3, 3, 3, 4, 4, 4, 4),
+     (0, 0, 3, 3, 3): (5, 5, 5, 5, 6, 6, 6, 6), (0, 0, 1, 3, 3): (5, 5, 5, 5, 7, 7, 7, 7),
+     (0, 1, 3, 3, 3): (5, 5, 5, 5, 6, 6, 6, 6), (0, 1, 1, 3, 3): (7, 7, 7, 7, 8, 8, 8, 8),
+     (0, 1, 1, 0, 3): (3, 3, 3, 3, 5, 5, 5, 5), (1, 1, 2, 2, 3): (7, 7, 7, 7, 8, 8, 8, 8),
+     (1, 0, 3, 3, 3): (5, 5, 5, 5, 6, 6, 6, 6), (1, 0, 0, 3, 3): (1, 1, 1, 1, 7, 7, 7, 7),
+     (2, 3, 3, 3, 3): (3, 3, 3, 3, 4, 4, 4, 4), (2, 1, 3, 3, 3): (5, 5, 5, 5, 6, 6, 6, 6),
+     (2, 1, 2, 3, 3): (3, 3, 3, 3, 7, 7, 7, 7), (2, 2, 3, 3, 3): (5, 5, 5, 5, 6, 6, 6, 6),
+     (2, 2, 2, 3, 3): (7, 7, 7, 7, 8, 8, 8, 8), (0, 2, 3, 3, 3): (5, 5, 5, 5, 6, 6, 6, 6),
+     (0, 2, 2, 3, 3): (7, 7, 7, 7, 8, 8, 8, 8), (2, 1, 1, 3, 3): (7, 7, 7, 7, 8, 8, 8, 8),
+     (2, 1, 1, 0, 3): (3, 3, 3, 3, 5, 5, 5, 5), (0, 0, 0, 3, 3): (7, 7, 7, 7, 8, 8, 8, 8),
+     (1, 2, 3, 3, 3): (5, 5, 5, 5, 6, 6, 6, 6), (1, 2, 0, 3, 3): (1, 1, 1, 1, 7, 7, 7, 7),
+     (0, 1, 2, 3, 3): (3, 3, 3, 3, 7, 7, 7, 7), (1, 1, 0, 3, 3): (1, 1, 1, 1, 3, 3, 3, 3),
+     (1, 1, 0, 2, 3): (7, 7, 7, 7, 8, 8, 8, 8), (1, 2, 2, 3, 3): (1, 1, 1, 1, 7, 7, 7, 7),
+     (1, 0, 1, 3, 3): (7, 7, 7, 7, 8, 8, 8, 8), (1, 0, 1, 0, 3): (1, 1, 1, 1, 5, 5, 5, 5),
+     (2, 2, 0, 3, 3): (7, 7, 7, 7, 8, 8, 8, 8), (1, 1, 1, 0, 3): (5, 5, 5, 5, 7, 7, 7, 7),
+     (2, 0, 3, 3, 3): (5, 5, 5, 5, 6, 6, 6, 6), (2, 0, 1, 3, 3): (5, 5, 5, 5, 7, 7, 7, 7),
+     (0, 1, 0, 3, 3): (3, 3, 3, 3, 7, 7, 7, 7), (2, 1, 1, 2, 3): (3, 3, 3, 3, 5, 5, 5, 5),
+     (1, 0, 1, 2, 3): (1, 1, 1, 1, 5, 5, 5, 5), (2, 1, 0, 3, 3): (3, 3, 3, 3, 7, 7, 7, 7),
+     (1, 0, 2, 3, 3): (1, 1, 1, 1, 7, 7, 7, 7), (2, 2, 1, 3, 3): (5, 5, 5, 5, 7, 7, 7, 7),
+     (0, 2, 0, 3, 3): (7, 7, 7, 7, 8, 8, 8, 8), (1, 2, 1, 3, 3): (7, 7, 7, 7, 8, 8, 8, 8),
+     (1, 2, 1, 0, 3): (1, 1, 1, 1, 5, 5, 5, 5), (0, 1, 1, 2, 3): (3, 3, 3, 3, 5, 5, 5, 5),
+     (1, 1, 1, 2, 3): (5, 5, 5, 5, 7, 7, 7, 7), (2, 0, 0, 3, 3): (7, 7, 7, 7, 8, 8, 8, 8),
+     (2, 0, 2, 3, 3): (7, 7, 7, 7, 8, 8, 8, 8), (1, 1, 0, 0, 3): (7, 7, 7, 7, 8, 8, 8, 8),
+     (0, 2, 1, 3, 3): (5, 5, 5, 5, 7, 7, 7, 7), (0, 0, 2, 3, 3): (7, 7, 7, 7, 8, 8, 8, 8),
+     (1, 2, 1, 2, 3): (1, 1, 1, 1, 5, 5, 5, 5)
+}
+
+create_decoder_schematic(input_data.keys(), "Decoder.schem")
+create_lut_schematic(input_data.values(), "LUT.schem")
